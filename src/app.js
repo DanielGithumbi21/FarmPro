66ebcb8d9f924a9f4049003ec82b2813bf9ecd00
Githumbi <danielgithumbi1998@gmail.com>
2020-06-02 05:39:12 +0300

    login update

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
-import './App.scss';
+import "./App.scss";
 
 class App extends Component {
-    state = {}
-    render() {
-        return (
-            <Router>
-                <Route path="/" exact component={Login} />
-                <Route path="/register" exact component={Register} />
-            </Router>
-        );
-    }
+  state = {};
+  render() {
+    return (
+      <Router>
+        <Route path="/" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,30 +1,69 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import './Login.scss';
-import { useForm } from 'react-hook-form';
+import "./Login.scss";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    const { handleSubmit, register, errors } = useForm();
-    const onSubmit = values => console.log(values);
-    return (
-        <div className="container-fluid">
-            <h1 className="display-4">FARMPRO</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-group">
-                    <label className="display-4" htmlFor="phone number">Phone Number:</label>
-                    <input type="text" name="phone" ref={register({ required: true, pattern: { value: 364327843232774832784237, message: "invalid email" } })} className="form-control" placeholder="phone number" />
-                    {errors.phone && <p style={{ color: "red" }}>Invalid phone number</p>}
-                </div>
-                <div className="form-group">
-                    <label className="display-4" htmlFor="password">password:</label>
-                    <p className="lead" style={{ color: "blue" }}>(Atleast 8 characters)</p>
-                    <input type="password" name="password" ref={register({ required: true, maxLength: 14, minLength: 8, pattern: { value: 3643278432327748327842 } })} placeholder="password" className="form-control" />
-                    {errors.password && <p style={{ color: "red" }}>Invalid password</p>}
-                </div>
-                <button className="btn btn-primary" type="submit">Submit</button>
-            </form>
-            <p className="lead"> Don't have an account?  <Link to="/register"><button type="button" className="btn btn-warning">Register</button></Link></p>
+  const { handleSubmit, register, errors } = useForm();
+  const onSubmit = (values) => console.log(values);
+  return (
+    <div className="container-fluid">
+      <h1 className="display-4">FARMPRO</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label className="display-4" htmlFor="phone number">
+            Phone Number:
+          </label>
+          <input
+            type="text"
+            name="phone"
+            ref={register({
+              required: true,
+              pattern: {
+                value: 364327843232774832784237,
+                message: "invalid email",
+              },
+            })}
+            className="form-control"
+            placeholder="phone number"
+          />
+          {errors.phone && <p style={{ color: "red" }}>Invalid phone number</p>}
         </div>
-    )
-}
+        <div className="form-group">
+          <label className="display-4" htmlFor="password">
+            password:
+          </label>
+          <p className="lead" style={{ color: "blue" }}>
+            (Atleast 8 characters)
+          </p>
+          <input
+            type="password"
+            name="password"
+            ref={register({
+              required: true,
+              maxLength: 14,
+              minLength: 8,
+              pattern: { value: 3643278432327748327842 },
+            })}
+            placeholder="password"
+            className="form-control"
+          />
+          {errors.password && <p style={{ color: "red" }}>Invalid password</p>}
+        </div>
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
+      </form>
+      <p className="lead">
+        {" "}
+        Don't have an account?{" "}
+        <Link to="/register">
+          <button type="button" className="btn btn-warning">
+            Register
+          </button>
+        </Link>
+      </p>
+    </div>
+  );
+};
 export default Login;
